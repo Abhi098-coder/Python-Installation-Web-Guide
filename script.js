cab4464122c1531eb74e6ff7f0464fc9329ebe88
Abhi098-coder <68606665+Abhi098-coder@users.noreply.github.com>
2024-06-01 12:20:33 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,83 @@
+$(document).ready(function() {
+    $("#step1Title").click(function () {
+        $("#step1Content").slideToggle("slow");
+    });
+    $("#step2Title").click(function () {
+        $("#step2Content").slideToggle("slow");
+    });
+    $("#step3Title").click(function () {
+        $("#step3Content").slideToggle("slow");
+    });
+    $("#step4Title").click(function () {
+        $("#step4Content").slideToggle("slow");
+    });
+    $("#step5Title").click(function () {
+        $("#step5Content").slideToggle("slow");
+    });
+    $("#step6Title").click(function () {
+        $("#step6Content").slideToggle("slow");
+    });
+    $("#step7Title").click(function () {
+        $("#step7Content").slideToggle("slow");
+    });
+    $("#step8Title").click(function () {
+        $("#step8Content").slideToggle("slow");
+    });
+    $("#step9Title").click(function () {
+        $("#step9Content").slideToggle("slow");
+    });
+    $("#step1Title").hover(function () {
+            $("#step1Title").css("background-color", "#bab4a2");
+        }, function () {
+            $("#step1Title").css("background-color", "#b0a586");
+        }
+    );
+    $("#step2Title").hover(function () {
+            $("#step2Title").css("background-color", "#bab4a2");
+        }, function () {
+            $("#step2Title").css("background-color", "#b0a586");
+        }
+    );
+    $("#step3Title").hover(function () {
+            $("#step3Title").css("background-color", "#bab4a2");
+        }, function () {
+            $("#step3Title").css("background-color", "#b0a586");
+        }
+    );
+    $("#step4Title").hover(function () {
+            $("#step4Title").css("background-color", "#bab4a2");
+        }, function () {
+            $("#step4Title").css("background-color", "#b0a586");
+        }
+    );
+    $("#step5Title").hover(function () {
+            $("#step5Title").css("background-color", "#bab4a2");
+        }, function () {
+            $("#step5Title").css("background-color", "#b0a586");
+        }
+    );
+    $("#step6Title").hover(function () {
+            $("#step6Title").css("background-color", "#bab4a2");
+        }, function () {
+            $("#step6Title").css("background-color", "#b0a586");
+        }
+    );
+    $("#step7Title").hover(function () {
+            $("#step7Title").css("background-color", "#bab4a2");
+        }, function () {
+            $("#step7Title").css("background-color", "#b0a586");
+        }
+    );
+    $("#step8Title").hover(function () {
+            $("#step8Title").css("background-color", "#bab4a2");
+        }, function () {
+            $("#step8Title").css("background-color", "#b0a586");
+        }
+    );
+    $("#step9Title").hover(function () {
+            $("#step9Title").css("background-color", "#bab4a2");
+        }, function () {
+            $("#step9Title").css("background-color", "#b0a586");
+        }
+    );
+});
